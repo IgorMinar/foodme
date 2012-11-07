3fb5d7e566c8c97f50b000c97c17655ea0ef3127
Vojta Jina <vojta.jina@gmail.com>
2012-11-07 11:48:01 +0000

    Wire the backend up

--- app/js/controllers/RestaurantsController.js
+++ app/js/controllers/RestaurantsController.js
@@ -1,6 +1,7 @@
 'use strict';
 
-foodMeApp.controller('RestaurantsController', function($scope, userInfo, $location) {
+foodMeApp.controller('RestaurantsController', function($scope, userInfo, $location, $resource) {
+  var Restaurant = $resource('/api/restaurant/:id', {id: '@id'});
 
   if (!userInfo.address) {
     $location.url('/user-info');
@@ -8,21 +9,6 @@ foodMeApp.controller('RestaurantsController', function($scope, userInfo, $locati
 
   $scope.userInfo = userInfo;
 
-  $scope.restaurants = [
-    { id: 'esthers2',
-      name: "Esther's German Saloon2",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut neque orci. Fusce bibendum mauris eu nulla tincidunt ultrices. Donec consequat adipiscing sapien non tincidunt. Aliquam varius suscipit blandit.",
-      rating: 5,
-      price: 4,
-      open : '012345'
-    },
-    { id: 'esthers',
-      name: "Esther's German Saloon",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut neque orci. Fusce bibendum mauris eu nulla tincidunt ultrices. Vestibulum leo velit, viverra quis pulvinar eu, aliquet vitae libero. Pellentesque facilisis felis ac ligula molestie eu congue lectus egestas.",
-      rating: 4,
-      price: 3,
-      open : '0123456'
-    }
-  ]
+  $scope.restaurants = Restaurant.query();
 
 });
