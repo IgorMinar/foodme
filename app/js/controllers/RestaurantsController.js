7e8a8e1a1b1a5534524df37df474ec4b21f72f55
Vojta Jina <vojta.jina@gmail.com>
2012-11-07 14:56:02 +0000

    Filtering (price, rating, cuisine)

--- app/js/controllers/RestaurantsController.js
+++ app/js/controllers/RestaurantsController.js
@@ -8,6 +8,38 @@ foodMeApp.controller('RestaurantsController', function($scope, userInfo, $locati
 
   $scope.userInfo = userInfo;
 
-  $scope.restaurants = Restaurant.query();
+  $scope.allRestaurants = Restaurant.query();
 
+  $scope.filter = {
+    cuisine: [],
+    price: null,
+    rating: null
+  };
+
+  var filterRestaurants = function() {
+    var restaurants = $scope.allRestaurants;
+    var filter = $scope.filter;
+
+    $scope.restaurants = [];
+
+    angular.forEach(restaurants, function(item, key) {
+
+      if (filter.cuisine.length && filter.cuisine.indexOf(item.cuisine) === -1) {
+        return;
+      }
+
+      if (filter.price && filter.price != item.price) {
+        return;
+      }
+
+      if (filter.rating && filter.rating != item.rating) {
+        return;
+      }
+
+      $scope.restaurants.push(item);
+    });
+  };
+
+  $scope.$watch('allRestaurants', filterRestaurants, true);
+  $scope.$watch('filter', filterRestaurants, true);
 });
