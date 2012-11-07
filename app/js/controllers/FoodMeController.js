0170ab9b3d4403193c50fb7835a283a1da94eec9
Vojta Jina <vojta.jina@gmail.com>
2012-11-07 21:34:41 +0000

    remove console.log

--- app/js/controllers/FoodMeController.js
+++ app/js/controllers/FoodMeController.js
@@ -2,7 +2,6 @@
 
 foodMeApp.controller('FoodMeController', function($scope, $location) {
   $scope.routeIs = function(routeName) {
-    console.log('xxx', $location.path())
     return $location.path() === routeName;
   };
 });
