bd2770475ae50ea419c2c058915a35c31b1ef4e5
Igor Minar <igor@angularjs.org>
2012-11-09 06:03:38 +0100

    fmDeliverTo directive

---
+++ app/js/directives/deliverTo.js
@@ -0,0 +1,10 @@
+foodMeApp.directive('fmDeliverTo', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'js/directives/deliverTo.html',
+    scope: {},
+    controller: function FmDeliverToController($scope, userInfo) {
+      $scope.userInfo = userInfo;
+    }
+  }
+});
