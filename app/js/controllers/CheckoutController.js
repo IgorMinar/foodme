40767374ace9ffe50af4a72dc7cf01238543bd2a
Igor Minar <igor@angularjs.org>
2012-11-09 06:00:38 +0100

    properly disable checkout button

--- app/js/controllers/CheckoutController.js
+++ app/js/controllers/CheckoutController.js
@@ -1,9 +1,10 @@
 'use strict';
 
-foodMeApp.controller('CheckoutController', function($scope, cart, userInfo) {
+foodMeApp.controller('CheckoutController', function($scope, cart, userInfo, $location) {
   $scope.cart = cart;
   $scope.restaurantId = cart.restaurant.id;
   $scope.user = userInfo;
+  $scope.submitting = false;
 
   $scope.cards = [
     { type:'visa', label:'Visa' },
