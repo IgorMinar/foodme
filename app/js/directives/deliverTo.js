0adb168a9a3b6e2f711111d0e1c76b534ff637a0
Igor Minar <igor@angularjs.org>
2012-11-10 13:47:58 +0100

    exercise 6 - solution

--- app/js/directives/deliverTo.js
+++ app/js/directives/deliverTo.js
@@ -1,1 +1,12 @@
 'use strict';
+
+foodMeApp.directive('fmDeliverTo', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'js/directives/deliverTo.html',
+    scope: {},
+    controller: function FmDeliverToController($scope, userInfo) {
+      $scope.userInfo = userInfo;
+    }
+  }
+});
