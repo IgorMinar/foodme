1db250f91c2c9c9d75ca63819c6e82fa5d2e3c2b
Igor Minar <igor@angularjs.org>
2012-11-10 05:14:21 +0100

    exercise 1 - solution

--- app/js/controllers/CustomerController.js
+++ app/js/controllers/CustomerController.js
@@ -1,10 +1,16 @@
 'use strict';
 
 foodMeApp.controller('CustomerController',
-    function CustomerController($scope, userInfo) {
+    function CustomerController($scope, userInfo, $location) {
+
+  $scope.customerName = userInfo.name;
+  $scope.address = userInfo.address;
 
 
   $scope.findRestaurants = function(customerName, address) {
+    userInfo.name = customerName;
+    userInfo.address = address;
 
+    $location.url('/');
   };
 });
