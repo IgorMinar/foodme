f35d446619a2ef44f29eeab9f2a1d5a2b44ac089
Vojta Jina <vojta.jina@gmail.com>
2012-11-10 12:04:59 +0100

    exercise 3 - solution

--- app/js/controllers/RestaurantsController.js
+++ app/js/controllers/RestaurantsController.js
@@ -1,16 +1,7 @@
 'use strict';
 
 foodMeApp.controller('RestaurantsController',
-    function RestaurantsController($scope) {
+    function RestaurantsController($scope, Restaurant) {
 
-  $scope.restaurants = [{
-    name: 'Angular Pizza', id: 'angular', price: 1, rating: 5,
-    description: 'Home of the superheroic pizza!'
-  }, {
-    name: 'BBQ Tofu Paradise', id: 'tofuparadise', price: 2, rating: 1,
-    description: 'Home of the superheroic pizza! Vegetarians, we have your BBQ needs covered. Our home-made tofu skewers and secret BBQ sauce will have you licking your fingers.'
-  }, {
-    name: 'Beijing Express', id: 'beijing', price: 2, rating: 4,
-    description: 'Fast, healthy, Chinese food. Family specials for takeout or delivery. Try our Peking Duck!'
-  }];
+  $scope.restaurants = Restaurant.query();
 });
--- app/js/services/Restaurant.js
+++ app/js/services/Restaurant.js
@@ -1,1 +1,5 @@
 'use strict';
+
+foodMeApp.factory('Restaurant', function($resource) {
+  return $resource('/api/restaurant/:id', {id: '@id'});
+});
