97b11a461471bae7d41b29ad71e61be174cbd3ce
Vojta Jina <vojta.jina@gmail.com>
2012-11-10 15:50:53 +0100

    exercise 4 - initial state

--- app/js/app.js
+++ app/js/app.js
@@ -8,30 +8,5 @@ foodMeApp.config(function($routeProvider) {
       when('/', {
         controller: 'RestaurantsController',
         templateUrl: 'views/restaurants.html'
-      }).
-      when('/menu/:restaurantId', {
-        controller: 'MenuController',
-        templateUrl: 'views/menu.html'
-      }).
-      when('/customer', {
-        controller: 'CustomerController',
-        templateUrl: 'views/customer.html'
-      }).
-      when('/checkout', {
-        controller: 'CheckoutController',
-        templateUrl: 'views/checkout.html'
-      }).
-      when('/thank-you', {
-        controller: 'ThankYouController',
-        templateUrl: 'views/thank-you.html'
-      }).
-      when('/who-we-are', {
-        templateUrl: 'views/who-we-are.html'
-      }).
-      when('/how-it-works', {
-        templateUrl: 'views/how-it-works.html'
-      }).
-      when('/help', {
-        templateUrl: 'views/help.html'
       });
 });
