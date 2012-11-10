88909572e3a7ceb509bd3b55f90ffae07eea1075
Vojta Jina <vojta.jina@gmail.com>
2012-11-10 12:01:59 +0100

    squash into initial

--- app/js/controllers/RestaurantsController.js
+++ app/js/controllers/RestaurantsController.js
@@ -8,7 +8,7 @@ foodMeApp.controller('RestaurantsController',
     description: 'Home of the superheroic pizza!'
   }, {
     name: 'BBQ Tofu Paradise', id: 'tofuparadise', price: 2, rating: 1,
-    description: 'Home of the superheroic pizza!Vegetarians, we have your BBQ needs covered. Our home-made tofu skewers and secret BBQ sauce will have you licking your fingers.'
+    description: 'Home of the superheroic pizza! Vegetarians, we have your BBQ needs covered. Our home-made tofu skewers and secret BBQ sauce will have you licking your fingers.'
   }, {
     name: 'Beijing Express', id: 'beijing', price: 2, rating: 4,
     description: 'Fast, healthy, Chinese food. Family specials for takeout or delivery. Try our Peking Duck!'
