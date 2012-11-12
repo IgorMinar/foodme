839fc1362659ee4c6ecd807b68375a8c0eb3230d
Igor Minar <igor@angularjs.org>
2012-11-12 10:55:15 +0100

    5 - customer route

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
 var foodMeApp = angular.module('foodMeApp', []);
+
+foodMeApp.config(function($routeProvider) {
+
+  $routeProvider.
+      when('/', {
+        controller: 'CustomerController',
+        templateUrl: 'views/customer.html'
+      });
+});
