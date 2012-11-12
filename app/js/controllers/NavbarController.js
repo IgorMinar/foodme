c2446af5590b1778b3f80d38e6d29c9503a22830
Igor Minar <igor@angularjs.org>
2012-11-12 11:11:14 +0100

    step-8 - navbar selection

---
+++ app/js/controllers/NavbarController.js
@@ -0,0 +1,9 @@
+'use strict';
+
+foodMeApp.controller('NavbarController', function NavbarController($scope, $location) {
+
+  $scope.routeIs = function(routeName) {
+    return $location.path() === routeName;
+  };
+
+});
