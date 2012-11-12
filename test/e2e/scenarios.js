035bc2a11dfc842dfac062a40bd96cfbf70cb45d
Misko Hevery <misko@hevery.com>
2012-11-13 00:23:36 +0100

    2 - tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,9 +2,24 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('foodme', function() {
 
   beforeEach(function() {
-    browser().navigateTo('../../app/index.html');
+    browser().navigateTo('/index.html');
+  });
+
+  it('should read delivery information', function() {
+    var alertText;
+
+    input('customerName').enter('Customer');
+    input('address').enter('Address');
+    this.addFutureAction('mack alert()', function(window, document, done) {
+      window.alert = function(text) { alertText = text; };
+      done();
+    });
+    element(':button.btn-primary').click();
+    expect(this.addFutureAction('alert text', function(window, decoment, done){
+      done(null, alertText);
+    })).toEqual('Customer - Address');
   });
 });
