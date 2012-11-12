2a43230dff38398065a19dda464f7d81390ffdd2
Igor Minar <igor@angularjs.org>
2012-11-12 23:43:54 +0100

    step-23 - fmCheckboxList directive and cuisine filter

---
+++ app/js/directives/fmCheckboxList.js
@@ -0,0 +1,34 @@
+'use strict';
+
+foodMeApp.directive('fmCheckboxList', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attr, ngModel) {
+
+      // model -> view
+      ngModel.$render = function(values) {
+        values = values || [];
+        angular.forEach(elm.find('input'), function(input) {
+          input.checked = values.indexOf(input.getAttribute('value')) !== -1;
+        });
+      };
+
+      // view -> model
+      elm.bind('click', function(e) {
+        if (angular.lowercase(e.target.nodeName) === 'input') {
+          scope.$apply(function() {
+            var values = [];
+
+            angular.forEach(elm.find('input'), function(input) {
+              if (input.checked) {
+                values.push(input.getAttribute('value'));
+              }
+            });
+
+            ngModel.$setViewValue(values);
+          });
+        }
+      });
+    }
+  };
+});
