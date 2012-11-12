1662dba485316a4550570f144a809aa191909420
Igor Minar <igor@angularjs.org>
2012-11-12 22:54:54 +0100

    19 - wire up rating and price filters

--- app/js/controllers/RestaurantsController.js
+++ app/js/controllers/RestaurantsController.js
@@ -7,12 +7,34 @@ foodMeApp.controller('RestaurantsController',
     $location.url('/customer');
   }
 
-  $scope.restaurants = Restaurant.query();
-
-  $scope.filter = {
+  var filter = $scope.filter = {
     cuisine: [],
     price: null,
     rating: null
+  };
+
+  var allRestaurants = Restaurant.query(filterRestaurants);
+  $scope.$watch('filter', filterRestaurants, true);
+
+  function filterRestaurants() {
+    $scope.restaurants = [];
+
+    // filter
+    angular.forEach(allRestaurants, function(item, key) {
+      if (filter.cuisine.length && filter.cuisine.indexOf(item.cuisine) === -1) {
+        return;
+      }
+
+      if (filter.price && filter.price !== item.price) {
+        return;
+      }
+
+      if (filter.rating && filter.rating !== item.rating) {
+        return;
+      }
+
+      $scope.restaurants.push(item);
+    });
   };
 
 });
