08316866515632fb6d2314b469ac2e05171a5701
Igor Minar <igor@angularjs.org>
2012-11-12 19:08:31 +0100

    15 - add fmDeliverTo directive

---
+++ app/js/directives/fmDeliverTo.js
@@ -0,0 +1,12 @@
+'use strict';
+
+foodMeApp.directive('fmDeliverTo', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'js/directives/fmDeliverTo.html',
+    scope: {},
+    controller: function FmDeliverToController($scope, customer) {
+      $scope.customer = customer;
+    }
+  }
+});
