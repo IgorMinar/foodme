aa8ed5325d3289d54d0744ab56b06ac0b23f93c8
Igor Minar <igor@angularjs.org>
2012-11-12 10:59:33 +0100

    step-7b - static routes

--- app/js/app.js
+++ app/js/app.js
@@ -8,5 +8,15 @@ foodMeApp.config(function($routeProvider) {
       when('/', {
         controller: 'CustomerController',
         templateUrl: 'views/customer.html'
+      }).
+      when('/who-we-are', {
+        templateUrl: 'views/who-we-are.html'
+      }).
+      when('/how-it-works', {
+        templateUrl: 'views/how-it-works.html'
+      }).
+      when('/help', {
+        templateUrl: 'views/help.html'
       });
+
 });
