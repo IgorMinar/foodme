3d16a453aea759b0f498cea79637cce3f7e7531a
Misko Hevery <misko@hevery.com>
2012-11-13 00:23:36 +0100

    step-2a - data-bind customer form (test)

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,9 +2,27 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('foodme', function() {
 
   beforeEach(function() {
-    browser().navigateTo('../../app/index.html');
+    browser().navigateTo('/index.html');
+  });
+
+  it('should read customer information and alert when button is clicked', function() {
+    var alertText;
+
+    input('customerName').enter('Customer');
+    input('customerAddress').enter('Address');
+
+    this.addFutureAction('mock alert()', function(window, document, done) {
+      window.alert = function(text) { alertText = text; };
+      done();
+    });
+
+    element(':button.btn-primary').click();
+
+    expect(this.addFutureAction('alert text', function(window, decoment, done){
+      done(null, alertText);
+    })).toEqual('Customer - Address');
   });
 });
