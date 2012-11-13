d3ad2c2372d6056efa56d1bdc5792c2e21e2ca73
Vojta Jina <vojta.jina@gmail.com>
2012-11-13 01:55:01 +0100

    step-15a - wire up the 'deliver to' info panel (test)

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -75,4 +75,22 @@ describe('foodme', function() {
       expect(element('.fm-restaurant-list h4').text()).toMatch('39 restaurants found');
     });
   });
+
+
+  describe('fmDeliverTo', function() {
+
+    beforeEach(function() {
+      browser().navigateTo('/index.html#/customer');
+
+      // fill in the customer, so that we navigate to restaurants list
+      input('customerName').enter('John');
+      input('customerAddress').enter('Some city');
+      element(':button.btn-primary').click();
+    });
+
+
+    it('should show deliver to address', function() {
+      expect(element('.breadcrumb').text()).toMatch('Some city');
+    });
+  });
 });
