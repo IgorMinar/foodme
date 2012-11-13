1ef9c50f1590371b98024d32d565e2bbf93b3fdb
Misko Hevery <misko@hevery.com>
2012-11-13 01:02:21 +0100

    6 - tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,27 +4,49 @@
 
 describe('foodme', function() {
 
-  beforeEach(function() {
-    browser().navigateTo('/index.html');
+  describe('customer', function() {
+    beforeEach(function() {
+      browser().navigateTo('/index.html');
+    });
+
+    it('should read delivery information', function() {
+      var alertText;
+
+      input('customerName').enter('Customer');
+      input('address').enter('Address');
+      element(':button.btn-primary').click();
+
+      browser().navigateTo('/index.html#/');
+
+      expect(input('customerName').val()).toEqual('Customer');
+      expect(input('address').val()).toEqual('Address');
+    });
+
+    it('should disable form submission button when empty', function() {
+      input('customerName').enter('');
+      input('address').enter('');
+
+      expect(element(":button.btn-primary:disabled").count()).toEqual(1);
+    });
+
   });
 
-  it('should read delivery information', function() {
-    var alertText;
+  describe("routes", function() {
+    it('should navigate to who we are', function() {
+      browser().navigateTo('/index.html#/who-we-are');
+      expect(element('ng-view').text()).
+          toContain('We are purple unicorns jockeys and knitters of woolen socks');
+    });
 
-    input('customerName').enter('Customer');
-    input('address').enter('Address');
-    element(':button.btn-primary').click();
+    it('should navigate to how it works', function() {
+      browser().navigateTo('/index.html#/how-it-works');
+      expect(element('ng-view').text()).toContain('It\'s simple:');
+    });
 
-    browser().navigateTo('/index.html#/');
-
-    expect(input('customerName').val()).toEqual('Customer');
-    expect(input('address').val()).toEqual('Address');
-  });
-
-  it('should disable form submission button when empty', function() {
-    input('customerName').enter('');
-    input('address').enter('');
-
-    expect(element(":button.btn-primary:disabled").count()).toEqual(1);
+    it('should navigate to help', function() {
+      browser().navigateTo('/index.html#/help');
+      expect(element('ng-view').text()).
+          toContain('Until how late do you deliver');
+    });
   });
 });
