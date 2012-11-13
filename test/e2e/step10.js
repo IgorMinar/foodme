58119fb3c75e90009c6a519a86440426ddfecbeb
Vojta Jina <vojta.jina@gmail.com>
2012-11-13 01:36:57 +0100

    step-12a - data-bind restaurants view (test)

---
+++ test/e2e/step10.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('step 10', function() {
+
+  beforeEach(function() {
+    browser().navigateTo('/index.html#/customer');
+
+    // fill in the customer, so that we navigate to restaurants list
+    input('customerName').enter('John');
+    input('customerAddress').enter('Some city');
+    element(':button.btn-primary').click();
+  });
+
+
+  it('should render 3 restaurants', function() {
+    var table = repeater('.fm-restaurant-list table tr');
+    expect(table.count()).toEqual(4);
+    expect(table.column('restaurant.name')).toEqual(['Angular Pizza', 'BBQ Tofu Paradise', 'Beijing Express']);
+  });
+});
