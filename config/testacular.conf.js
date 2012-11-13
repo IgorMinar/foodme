cd869f50d4799add8be87fad487f703bbc415883
Misko Hevery <misko@hevery.com>
2012-11-13 02:03:11 +0100

    merge testacular.conf.js

--- config/testacular.conf.js
+++ config/testacular.conf.js
@@ -6,6 +6,7 @@ files = [
   'app/lib/angular/angular.js',
   'app/lib/angular/angular-*.js',
   'test/lib/angular/angular-mocks.js',
+  'app/js/app.js',
   'app/js/**/*.js',
   'test/unit/**/*.js'
 ];
