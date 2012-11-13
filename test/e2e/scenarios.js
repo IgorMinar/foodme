aae503c55e78191a7f3aa6b2948c19db2d272a01
Misko Hevery <misko@hevery.com>
2012-11-13 01:43:12 +0100

    step-6a - customer route (test)

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,28 +4,30 @@
 
 describe('foodme', function() {
 
-  beforeEach(function() {
-    browser().navigateTo('/index.html');
-  });
+  describe('customer', function() {
+    beforeEach(function() {
+      browser().navigateTo('/index.html');
+    });
 
-  it('should persist customer information', function() {
-    input('customerName').enter('Customer');
-    input('customerAddress').enter('Address');
+    it('should persist customer information', function() {
+      input('customerName').enter('Customer');
+      input('customerAddress').enter('Address');
 
-    element(':button.btn-primary').click();
+      element(':button.btn-primary').click();
 
-    //reload the page
-    browser().navigateTo('/index.html#/');
+      // reload the page
+      browser().navigateTo('/index.html#/');
 
-    expect(input('customerName').val()).toEqual('Customer');
-    expect(input('customerAddress').val()).toEqual('Address');
-  });
+      expect(input('customerName').val()).toEqual('Customer');
+      expect(input('customerAddress').val()).toEqual('Address');
+    });
 
 
-  it('should disable form submission button when empty', function() {
-    input('customerName').enter('');
-    input('customerAddress').enter('');
+    it('should disable form submission button when empty', function() {
+      input('customerName').enter('');
+      input('customerAddress').enter('');
 
-    expect(element(':button.btn-primary:disabled').count()).toEqual(1);
+      expect(element(':button.btn-primary:disabled').count()).toEqual(1);
+    });
   });
 });
