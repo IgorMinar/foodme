bcdae884119b78fbd6ec9791ebbcf41f8683185a
Igor Minar <igor@angularjs.org>
2012-11-13 01:50:35 +0100

    step-27 - wire up checkout view

--- app/js/controllers/CheckoutController.js
+++ app/js/controllers/CheckoutController.js
@@ -1,4 +1,20 @@
 'use strict';
 
-foodMeApp.controller('CheckoutController', function CheckoutController() {
+foodMeApp.controller('CheckoutController',
+    function CheckoutController($scope, cart, customer, $location) {
+
+  $scope.cart = cart;
+  $scope.restaurantId = cart.restaurant.id;
+  $scope.customer = customer;
+  $scope.submitting = false;
+
+
+  $scope.purchase = function() {
+    if ($scope.submitting) return;
+
+    $scope.submitting = true;
+    cart.submitOrder().then(function(orderId) {
+      $location.path('thank-you').search({orderId: orderId});
+    });
+  };
 });
