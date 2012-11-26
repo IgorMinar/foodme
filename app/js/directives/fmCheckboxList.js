1b1366214bafc3d4752491c2d0aaf3b967995313
Vojta Jina <vojta.jina@gmail.com>
2012-11-26 21:14:59 +0100

    step-23c - solution

--- app/js/directives/fmCheckboxList.js
+++ app/js/directives/fmCheckboxList.js
@@ -1,5 +1,34 @@
 'use strict';
 
 foodMeApp.directive('fmCheckboxList', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attr, ngModel) {
 
+      // model -> view
+      ngModel.$render = function() {
+        var values = ngModel.$modelValue || [];
+        angular.forEach(elm.find('input'), function(input) {
+          input.checked = values.indexOf(input.getAttribute('value')) !== -1;
+        });
+      };
+
+      // view -> model
+      elm.bind('click', function(e) {
+        if (angular.lowercase(e.target.nodeName) === 'input') {
+          scope.$apply(function() {
+            var values = [];
+
+            angular.forEach(elm.find('input'), function(input) {
+              if (input.checked) {
+                values.push(input.getAttribute('value'));
+              }
+            });
+
+            ngModel.$setViewValue(values);
+          });
+        }
+      });
+    }
+  };
 });
